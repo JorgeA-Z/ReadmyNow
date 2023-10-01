6826be54b21069b7f740151a85d9f30d72048ba0
Henry0406 <enoc0406@hotmail.com>
2023-09-30 18:55:38 -0600

    New Loading.vue

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -3,88 +3,108 @@ import Text from '../components/Text.vue';
 </script>
 
 <template>
-
-<div class="vh-100 row m-0 align-items-center justify-content-center todo">
+  <div class="vh-100 row m-0 align-items-center justify-content-center todo">
     <div class="col-auto">
 
       <div class="row justify-content-center posicion">
 
-        <span class="loader">ReadMeN &nbsp; w!</span>
+        <div class="cont">
+          <h1 class="title">ReadMeNow!</h1>
+        </div>
+
+        <span class="loader"></span>
 
       </div>
 
     </div>
   </div>
-
 </template>
 
 <style scoped>
+.todo {
+  top: 0;
+  position: fixed;
+  z-index: 99;
+  height: 100%;
+  width: 100%;
+  background-color: white;
+  display: grid;
+  left: 0;
 
-.todo
+}
+
+.cont
 {
-    top: 0;
-    position: fixed;
-    z-index: 99;
-    height: 100%;
-    width: 100%;
-    background-color: white;
-    display: grid;
-    left: 0;
+  display: flex;
+  justify-content: center;
+}
 
+.title {
+  color: #7A60A9;
+  font-family: 'Comfortaa', cursive;
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.375px;
 }
 
 /* Animation */
 .loader {
-  display: inline-block;
-  font-size: 48px;
-  font-family: 'Comfortaa', cursive;
-  font-weight: bold;
-  color: #D6C8E1;
+  width: 200px;
+  height: 140px;
+  background: #7A60A9;
+  box-sizing: border-box;
   position: relative;
+  border-radius: 8px;
+  perspective: 1000px;
 }
 
-.loader::before {
-  content: '';  
+.loader:before {
+  content: '';
   position: absolute;
-  /* Position */
-  left: 267px;
-  top: 21px;
-  /* Size */
-  width: 32px;
-  height: 32px;
-  border-radius: 50%;
-  border: 5px solid #D6C8E1;
-  border-bottom-color: #7A60A9;
-  box-sizing: border-box;
-  animation: rotation 0.6s linear infinite;
+  left: 10px;
+  right: 10px;
+  top: 10px;
+  bottom: 10px;
+  border-radius: 8px;
+  background: #f5f5f5 no-repeat;
+  background-size: 60px 10px;
+  background-image: linear-gradient(#ddd 100px, transparent 0),
+    linear-gradient(#ddd 100px, transparent 0),
+    linear-gradient(#ddd 100px, transparent 0),
+    linear-gradient(#ddd 100px, transparent 0),
+    linear-gradient(#ddd 100px, transparent 0),
+    linear-gradient(#ddd 100px, transparent 0);
+
+  background-position: 15px 30px, 15px 60px, 15px 90px,
+    105px 30px, 105px 60px, 105px 90px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
 }
 
-@keyframes rotation {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}   
-
-@media screen and (max-width: 450px)
-{
-  .loader::before {
-  content: '';  
+.loader:after {
+  content: '';
   position: absolute;
-  /* Position */
-  left: 270px;
-  top: 21px;
-  /* Size */
-  width: 32px;
-  height: 32px;
-  border-radius: 50%;
-  border: 5px solid #D6C8E1;
-  border-bottom-color: #7A60A9;
-  box-sizing: border-box;
-  animation: rotation 0.6s linear infinite;
-  }
+  width: calc(50% - 10px);
+  right: 10px;
+  top: 10px;
+  bottom: 10px;
+  border-radius: 8px;
+  background: #fff no-repeat;
+  background-size: 60px 10px;
+  background-image: linear-gradient(#ddd 100px, transparent 0),
+    linear-gradient(#ddd 100px, transparent 0),
+    linear-gradient(#ddd 100px, transparent 0);
+  background-position: 50% 30px, 50% 60px, 50% 90px;
+  transform: rotateY(0deg);
+  transform-origin: left center;
+  animation: paging 1s linear infinite;
 }
 
+
+@keyframes paging {
+  to {
+    transform: rotateY(-180deg);
+  }
+}
 </style>
